05bf50a46c11601524313cb1eed5e0952e208bc3
Antonio Tan-Torres <tantonio@stanford.edu>
2018-05-16 17:50:21 -0700

    formatting

--- mrcnn/wad_dataset.py
+++ mrcnn/wad_dataset.py
@@ -6,34 +6,33 @@ import random
 
 
 class WadDataset(utils.Dataset):
-    """Generates the Amazon Shelf dataset.
-    """
+	"""Generates the WAD dataset.
+	"""
 
-    #self.rootdir = '/Users/antoniotantorres/workspace/cs230/project/benchmark/office/test/shelf'
-    def get_filelist(datadir,mode):
-    	np.random.seed(231)
-    	files = os.listdir(datadir)
-    	np.random.shuffle(files)
-    	filelist = []
-    	if mode == 'train':
-    		filelist = files[:-1000]
-    	elif mode == 'val':
-    		filelist = files[-1000:]
-    	return filelist
+	def get_filelist(datadir,mode):
+		np.random.seed(231)
+		files = os.listdir(datadir)
+		np.random.shuffle(files)
+		filelist = []
+		if mode == 'train':
+			filelist = files[:-1000]
+		elif mode == 'val':
+			filelist = files[-1000:]
+		return filelist
 
-    def __init__(self,class_map=None):
-        self._image_ids = []
-        self.image_info = []
-        # Background is always the first class
-        #self.class_info = [{"source": "", "id": 0, "name": "BG"}]
-        #self.source_class_ids = {}
-        self.datadir = '/home/antoniotantorres/project/cvpr-2018-autonomous-driving'
-        self.object_map = {'BG':0,'car':1,'motorcycle':2,'bicycle':3,'person':4,
-        					'truck':5,'bus':6,'tricycle':7,}
-    	self.filelist = get_filelist(self.datadir,'train')
+	def __init__(self,class_map=None):
+		self._image_ids = []
+		self.image_info = []
+		# Background is always the first class
+		#self.class_info = [{"source": "", "id": 0, "name": "BG"}]
+		#self.source_class_ids = {}
+		self.datadir = '/home/antoniotantorres/project/cvpr-2018-autonomous-driving'
+		self.object_map = {'BG':0,'car':1,'motorcycle':2,'bicycle':3,'person':4,
+							'truck':5,'bus':6,'tricycle':7,}
+		self.filelist = get_filelist(self.datadir,'train')
 
-     def get_masks(self,label_im):
-	 	"""Return lists of binary masks and ids corresponding to instances.
+	def get_masks(self,label_im):
+		"""Return lists of binary masks and ids corresponding to instances.
 	Params:
 	    label_im - label mask image from WAD as 2D numpy array
 	Returns:
@@ -63,63 +62,63 @@ class WadDataset(utils.Dataset):
 				masks.append(mask)
 		return ids, masks
 
-    def load_wad(self):
-        """Generate the requested number of synthetic images.
-        count: number of images to generate.
-        height, width: the size of the generated images.
-        """
+	def load_wad(self):
+		"""Generate the requested number of synthetic images.
+		count: number of images to generate.
+		height, width: the size of the generated images.
+		"""
 
-        x_train_dir = os.path.join(self.datadir, 'train_color')
-        y_train_dir = os.path.join(self.datadir, 'train_label')
+		x_train_dir = os.path.join(self.datadir, 'train_color')
+		y_train_dir = os.path.join(self.datadir, 'train_label')
 
-        # Add classes
-        for objName,class_num in self.object_map.items():
-            self.add_class("wad", class_num, objName)
+		# Add classes
+		for objName,class_num in self.object_map.items():
+		    self.add_class("wad", class_num, objName)
 
-        # Add images
-        image_id = 0
-        for filename in self.filelist:
-    		if filename.endswith(".jpg"): 
-    			rootname = filename[:-4]
+		# Add images
+		image_id = 0
+		for filename in self.filelist:
+			if filename.endswith(".jpg"): 
+				rootname = filename[:-4]
 
-    			xfilepath = os.path.join(x_train_dir,filename)
-    			yfilepath = os.path.join(y_train_dir,rootname+'.png')
-    			self.add_image('wad',image_id = image_id, xfilepath = xfilepath, rootname = rootname, yfilepath = yfilepath)
-	            image_id = image_id+1
+				xfilepath = os.path.join(x_train_dir,filename)
+				yfilepath = os.path.join(y_train_dir,rootname+'.png')
+				self.add_image('wad',image_id = image_id, xfilepath = xfilepath, rootname = rootname, yfilepath = yfilepath)
+				image_id = image_id+1
 
-    def load_image(self, image_id):
-        """Generate an image from the specs of the given image ID.
-        Typically this function loads the image from a file, but
-        in this case it generates the image on the fly from the
-        specs in image_info.
-        """
-        info = self.image_info[image_id]
-        img_path = info['xfilepath']
-        #print(img_path)
-        image = skimage.io.imread(img_path)
+	def load_image(self, image_id):
+		"""Generate an image from the specs of the given image ID.
+		Typically this function loads the image from a file, but
+		in this case it generates the image on the fly from the
+		specs in image_info.
+		"""
+		info = self.image_info[image_id]
+		img_path = info['xfilepath']
+		#print(img_path)
+		image = skimage.io.imread(img_path)
 
-        return image
+		return image
 
-    def image_reference(self, image_id):
-        """Return the wad data of the image."""
-        info = self.image_info[image_id]
-        if info["source"] == "wad":
-            return info
-        else:
-            super(self.__class__).image_reference(self, image_id)
+	def image_reference(self, image_id):
+		"""Return the wad data of the image."""
+		info = self.image_info[image_id]
+		if info["source"] == "wad":
+			return info
+		else:
+		super(self.__class__).image_reference(self, image_id)
 
-    def load_mask(self, image_id):
-        """Generate instance masks for shapes of the given image ID.
-        """
-        info = self.image_info[image_id]
-        img_path = info['xfilepath']
-        img_rootname = info['rootname']
-        segm_img_path = info['yfilepath']
-        segm_img = skimage.io.imread(segm_img_path)
+	def load_mask(self, image_id):
+		"""Generate instance masks for shapes of the given image ID.
+		"""
+		info = self.image_info[image_id]
+		img_path = info['xfilepath']
+		img_rootname = info['rootname']
+		segm_img_path = info['yfilepath']
+		segm_img = skimage.io.imread(segm_img_path)
 
-        class_ids,segm_masks = self.get_masks(segm_img)
-        # Map class names to class IDs.
-        return segm_masks, class_ids.astype(np.int32)
+		class_ids,segm_masks = self.get_masks(segm_img)
+		# Map class names to class IDs.
+		return segm_masks, class_ids.astype(np.int32)
 
 #dataset_train = WadDataset()
 #dataset_train.load_wad()
